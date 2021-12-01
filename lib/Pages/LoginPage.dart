6c72fe01605df731ef3d2bdddeecfc64e99a5999
salemgzouni <39351926+salemgzouni@users.noreply.github.com>
2021-12-01 18:21:52 +0100

    Login page

---
+++ lib/Pages/LoginPage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:gstock/components/Header.dart';
+import 'package:gstock/components/InputWrapper.dart';
+import '';
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter,colors:[
+            Colors.cyan,
+            Colors.blue,
+            Colors.black12
+          ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Header(),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60),
+                  topRight: Radius.circular(60),
+                )
+              ),
+              child: InputWrapper(),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
