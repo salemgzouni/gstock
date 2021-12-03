fa12fa83f09559efe7884328e0f2277ce81a13ed
salemgzouni <39351926+salemgzouni@users.noreply.github.com>
2021-12-03 19:29:32 +0100

    Models

---
+++ lib/Models/composant.dart
@@ -0,0 +1,46 @@
+class Composant{
+  late String _id;
+  late String _des;
+  late String _description;
+  late int _qte;
+
+  Composant(this._id, this._des, this._description, this._qte);
+
+  int get qte => _qte;
+
+  String get description => _description;
+
+  String get des => _des;
+
+  String get id => _id;
+
+  set qte(int value) {
+    _qte = value;
+  }
+
+  set description(String value) {
+    _description = value;
+  }
+
+  set des(String value) {
+    _des = value;
+  }
+  //convert composant Object to Map Object
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    if(id!=null){
+      map["id"] = _id;
+    }
+    map["des"] = _des;
+    map["description"] = _description;
+    map["qte"] = _qte;
+    return map;
+  }
+  //extract composant object from Map Object
+  Composant.map(dynamic obj) {
+    this._id = obj['id'];
+    this._des = obj['des'];
+    this._description = obj['description'];
+    this._qte = obj['qte'];
+  }
+}
