e8dddaef1f8b2fc30cf1fa37d6a7a00aab54a353
salemgzouni <39351926+salemgzouni@users.noreply.github.com>
2021-12-03 19:29:32 +0100

    Models

---
+++ lib/Models/Admin.dart
@@ -0,0 +1,56 @@
+class Admin{
+  late String _id;
+  late String _nom;
+  late String _prenom;
+  late String _email;
+  late String _mdp;
+
+  Admin(this._id, this._nom, this._prenom, this._email, this._mdp);
+
+  String get id => _id;
+
+  String get nom => _nom;
+
+  String get prenom => _prenom;
+
+  String get email => _email;
+
+  String get mdp => _mdp;
+
+  set mdp(String value) {
+    _mdp = value;
+  }
+
+  set email(String value) {
+    _email = value;
+  }
+
+  set prenom(String value) {
+    _prenom = value;
+  }
+
+  set nom(String value) {
+    _nom = value;
+  }
+
+  //convert Admin Object to Map Object
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    if(id!=null){
+      map["id"] = _id;
+    }
+    map["nom"] = _nom;
+    map["prenom"] = _prenom;
+    map["email"] = _email;
+    map["mdp"]=_mdp;
+    return map;
+  }
+  //extract admin object from Map Object
+  Admin.map(dynamic obj) {
+    this._id = obj['id'];
+    this._nom = obj['nom'];
+    this._prenom = obj['prenom'];
+    this._email = obj['email'];
+    this._mdp= obj['mdp'];
+  }
+}
---
+++ lib/Models/famille.dart
@@ -0,0 +1,49 @@
+import 'package:gstock/Models/composant.dart';
+
+class Famille{
+  late String _id;
+  late String _des;
+  late String _description;
+  late List<Composant> _listComposants;
+
+  Famille(this._id, this._des, this._description, this._listComposants);
+
+  List<Composant> get listComposants => _listComposants;
+
+  String get description => _description;
+
+  String get des => _des;
+
+  String get id => _id;
+
+
+  set listComposants(List<Composant> value) {
+    _listComposants = value;
+  }
+
+  set description(String value) {
+    _description = value;
+  }
+
+  set des(String value) {
+    _des = value;
+  }
+  //convert composant Object to Map Object
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    if(id!=null){
+      map["id"] = _id;
+    }
+    map["des"] = _des;
+    map["description"] = _description;
+    map["listComposants"] = _listComposants;
+    return map;
+  }
+  //extract composant object from Map Object
+  Famille.map(dynamic obj) {
+    this._id = obj['id'];
+    this._des = obj['des'];
+    this._description = obj['description'];
+    this._listComposants = obj['listComposants'];
+  }
+}
