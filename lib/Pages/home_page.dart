16737c2becd62df10acb7cd72705393df53a51e7
AnasHidri <93081714+AnasHidri@users.noreply.github.com>
2021-12-30 14:18:01 +0100

    interface homepage

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,66 +1,60 @@
+// TODO Implement this library.
 import 'package:flutter/material.dart';
-
+import 'home_page.dart';
 import 'ProfilePage.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
-  HomePage();
+
 }
 
 class _HomePageState extends State<HomePage> {
+  final titles = ["List 1", "List 2", "List 3"];
+  final subtitles = [
+    "Here is list 1 subtitle",
+    "Here is list 2 subtitle",
+    "Here is list 3 subtitle"
+  ];
+  final icons = [Icons.ac_unit, Icons.access_alarm, Icons.access_time];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("HomePage"),
-          automaticallyImplyLeading: true,
+          title: Text('test test aywa aywa'),
+          leading : IconButton(
+            icon: Icon (
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()),);
+            },
+          ),
+          actions:[
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings, color: Colors.white))
+          ]
       ),
-      body: Center(
-          child:  Text("Welcome, select a family please."),
+      body: Container(
+          child : ListView.builder(
+              itemCount: titles.length,
+              itemBuilder: (context, index) {
+                return Card(
+                    child: ListTile(
+                        onTap: () {
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(titles[index] + ' pressed!'),
+                          ));
+                        },
+                        title: Text(titles[index]),
+                        subtitle: Text(subtitles[index]),
+                        leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
+                        trailing: Icon(icons[index])));
+              })
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: const <Widget> [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-              ),
-              child: Text(
-                'list of family',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-
-            ListTile(
-              title: Text('test1',
-                  style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,)
-              ),
-            ),
-            ListTile(
-              title: Text('test1',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,)
-              ),
-            ),
-            ListTile(
-              title: Text('test1',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,)
-              ),
-            ),
-          ],
-        ),
-      ),
-
     );
-
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: HomePage(),
 
     );
 
