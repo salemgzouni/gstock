33ee7b3bb839a3f316a0b484a460cd01ac048b58
AnasHidri <93081714+AnasHidri@users.noreply.github.com>
2021-12-30 16:28:18 +0100

    interface homepage

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,11 +1,7 @@
 // TODO Implement this library.
 import 'package:flutter/material.dart';
-import 'package:gstock/Data/famille_operation.dart';
-import 'package:gstock/Models/famille.dart';
-import 'package:gstock/Pages/Composant.dart';
 import 'home_page.dart';
 import 'ProfilePage.dart';
-import '';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,21 +10,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final titles = ["List 1", "List 2", "List 3"];
+  final subtitles = [
+    "Here is list 1 subtitle",
+    "Here is list 2 subtitle",
+    "Here is list 3 subtitle"
+  ];
   final icons = [Icons.ac_unit, Icons.access_alarm, Icons.access_time];
   @override
   Widget build(BuildContext context) {
-    FamilleOperations familleOperations= new FamilleOperations();
-    //var  allFamilles= familleOperations.getAllFamilles() as List;
-    final titles = ["List 1", "List 2", "List 3"];
-    final subtitles = [
-      "Here is list 1 subtitle",
-      "Here is list 2 subtitle",
-      "Here is list 3 subtitle"
-    ];
-
     return Scaffold(
       appBar: AppBar(
-          title: Text('Les familles'),
+          title: Text('test test aywa aywa'),
           leading : IconButton(
             icon: Icon (
               Icons.arrow_back,
@@ -43,25 +36,24 @@ class _HomePageState extends State<HomePage> {
           ]
       ),
       body: Container(
-          child: FutureBuilder(
-            future:familleOperations.getAllFamilles(),
-            builder:(BuildContext context,AsyncSnapshot<dynamic> snapshot){
-            if(snapshot.hasData) {
-              final familleList = snapshot.data as List<Famille>;
-              return ListView.builder(
-                  itemCount: familleList.length,
-                  itemBuilder: (context, index) {
-                    return Text(familleList[index].des);
-                  });
-            }
-            else if (!snapshot.hasData){
-              return const Text('monji');
-            }
-            else {
-                return CircularProgressIndicator(); // displays while loading data
-              }
-            })
-        )
+          child : ListView.builder(
+              itemCount: titles.length,
+              itemBuilder: (context, index) {
+                return Card(
+                    child: ListTile(
+                        onTap: () {
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(titles[index] + ' pressed!'),
+                          ));
+                        },
+                        title: Text(titles[index]),
+                        subtitle: Text(subtitles[index]),
+                        leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
+                        trailing: Icon(icons[index])));
+              })
+      ),
     );
   }
 }
